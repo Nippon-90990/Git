f337c9f28122749e5e2a82c18c2adf71960c1a2d
Nippon-90990 <nipponbiswas90990@gmail.com>
2023-01-28 20:41:45 +0530

    Github

---
+++ pages/Mugs.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const TShirts = () => {
+  return (
+    <div>
+      <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p class="mt-1">₹ 16.00</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p class="mt-1">₹ 21.15</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p class="mt-1">₹ 12.00</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p class="mt-1">₹ 18.40</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p class="mt-1">₹ 16.00</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p class="mt-1">₹ 21.15</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class="m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p class="mt-1">₹ 12.00</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg rounded-3xl ">  {/* IF YPU WANT GIVE M-5 FOR MARGIN */}
+        <a class="block relative rounded overflow-hidden">
+          <img alt="ecommerce" class=" m-auto block" src="https://m.media-amazon.com/images/I/61JuR0uX6+L._AC_UY218_.jpg" />
+        </a>
+        <div class="mt-4 text-center">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p class="mt-1">₹ 18.40</p>
+          <p class="mt-1">S , M , L , XL , XXL</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  )
+}
+
+export default TShirts
